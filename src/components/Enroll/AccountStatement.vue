af930ad4cf82d398e9f9a8454a8f497bb3b230a9
Eduardo Castro <educastro2610@gmail.com>
2023-07-28 18:19:00 -0600

    AMS-136 Actualizar vista de Payment mediante store

--- src/components/Enroll/AccountStatement.vue
+++ src/components/Enroll/AccountStatement.vue
@@ -15,11 +15,20 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in payments" :key="item.name">
-            <td class="text-left">{{ item.name }}</td>
-            <td class="text-left">{{ item.value }}</td>
-            <td class="text-left">{{ item.paid }}</td>
-            <td class="text-left">{{ item.bank }}</td>
+          <tr>
+            <td class="text-left">
+              Matricula {{ store.user.student.periodIndex }}-PAC
+            </td>
+            <td class="text-left">270</td>
+            <td class="text-left d-flex">
+              <v-checkbox
+                v-if="store.user.student.payment"
+                :model-value="true"
+                disabled
+              ></v-checkbox>
+              <v-checkbox v-else :model-value="false" disabled></v-checkbox>
+            </td>
+            <td class="text-left">1</td>
           </tr>
         </tbody>
       </v-table>
@@ -33,14 +42,14 @@
 
 <script setup>
 import { ref } from "vue";
-import json from "../../mock/payments.json";
 const dialog = ref(false);
 const payments = ref(json);
 
 function TotalPay() {
   let pay = 0;
-  for (let i = 0; i < this.payments.length; i++) {
-    pay += this.payments[i].value;
+  if (!store.user.student.payment) {
+    pay = 270;
+    return pay;
   }
   return pay;
 }
