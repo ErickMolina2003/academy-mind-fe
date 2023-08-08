a0cb09c5724b46b8cb19339b64f48724aad27aa5
HanselHrz <hansel.hernandezc@gmail.com>
2023-08-08 10:23:06 -0600

    Falta cambiar el endpoint

--- src/components/DepartmentBoss/EnrolledStudents.vue
+++ src/components/DepartmentBoss/EnrolledStudents.vue
@@ -1,86 +1,89 @@
 <template>
-    <v-container>
-        <SearchableNavBar title="Estudiantes Matriculados" label="No.Cuenta"/>
-        <div>
-            <h2 style="padding-bottom: 15px">Lista de Estudiantes</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Número de cuenta</th>
-                        <!-- <th>Carrera</th> -->
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="student in originalStudents" :key="student.student.accountNumber">
-                        <td>{{ student.student.user.firstName}} {{student.student.user.secondName}} {{student.student.user.firstLastName}} {{student.student.user.secondLastName}}</td>
-                        <td>{{ student.student.accountNumber }}</td>
-                        <!-- <td>{{ student. }}</td> -->
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </v-container>
+  <v-container>
+    <SearchableNavBar title="Estudiantes Matriculados" label="No.Cuenta" />
+    <div>
+      <h2
+        style="
+          padding-bottom: 15px;
+          background-color: rgb(var(--v-theme-secondary-lighthen-1));
+        "
+        class="text-center text-white"
+      >
+        Lista de Estudiantes
+      </h2>
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Nombre</th>
+            <th>Número de cuenta</th>
+            <th>Correo Institucional</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(student, index) in paginatedStudents"
+            :key="student.student.accountNumber"
+          >
+            <td>{{ index + startIndex + 1 }}</td>
+            <td>
+              {{ student.student.user.firstName }}
+              {{ student.student.user.secondName }}
+              {{ student.student.user.firstLastName }}
+              {{ student.student.user.secondLastName }}
+            </td>
+            <td>{{ student.student.accountNumber }}</td>
+            <td>{{ student.student.institutionalEmail }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <v-pagination
+      v-model="currentPage"
+      :total-visible="5"
+      :length="totalPages"
+    />
+  </v-container>
 </template>
 
-<script setup lang="ts">
-import { onMounted, ref } from "vue";
+<script setup>
+import { onMounted, ref, computed, watch } from "vue";
 import SearchableNavBar from "@/components/NavBars/SearchableNavBar.vue";
-import SectionService from "@/services/section/section.service";
 import PeriodService from "@/services/period/period.service";
 import TuitionService from "@/services/tuition/tuition.service";
 
-const periodData = ref({});
-const state = ref("");
-
-const sectionService = new SectionService();
 const servicePeriod = new PeriodService();
 const serviceTuition = new TuitionService();
-
 const originalStudents = ref([]);
 const students = ref([]);
+const periods = ref([]);
+const periodToModify = ref({});
 
-onMounted(async() => {
-
-periodData.value = await getRecentPeriod();
-state.value = periodData.value.idStatePeriod.name;
-
-if (state.value == 'Matricula' || state.value == 'En curso' || state.value == 'Ingreso de notas' ) {
-    getStudentsPeriod(periodData.value.id);
-} else {
-    store.setToaster({
-        isActive: true,
-        text: "El perido actual no está en estado de planificación académica.",
-        color: "error",
-    });
-}
-
+onMounted(async () => {
+  getPeriods();
 });
 
-
-async function getStudentsPeriod(idPeriod: number) {
-  const response = await serviceTuition.getTuitionsStudentByPeriod(idPeriod);
-  originalStudents.value = response.registrations;
+async function getPeriods() {
+  const response = await servicePeriod.getPeriodsByYear(
+    new Date().getFullYear()
+  );
+  periods.value = response.periods;
+  periodToModify.value = periods.value[0];
+  if (
+    periodToModify.value.idStatePeriod?.name !== "Finalizado" &&
+    periodToModify.value.idStatePeriod?.name !== "Por definir"
+  ) {
+    console.log(periodToModify.value.id);
+    getStudentsPeriod(periodToModify.value.id);
+  }
 }
 
-async function getRecentPeriod() {
-    let mostRecentPeriod = null;
-    const response = await servicePeriod.getPeriods();
-    const periods = response.periods;
-    const currentYear = new Date().getFullYear();
-    const mostRecentYear = Math.max(...periods.map((period) => period.year));
-    const mostRecentPeriodNumber = Math.max(
-        ...periods
-            .filter((period) => period.year === mostRecentYear)
-            .map((period) => period.numberPeriod)
-    );
-    mostRecentPeriod = periods.find(
-        (period) =>
-            period.year === mostRecentYear && period.numberPeriod === mostRecentPeriodNumber
-    );
-    return mostRecentPeriod;
-};
-
+async function getStudentsPeriod(idPeriod) {
+  const response = await serviceTuition.getTuitionsStudentByPeriod(idPeriod);
+  originalStudents.value = response.registrations;
+  students.value = [...originalStudents.value]; // Actualizar la lista students
+}
 
 const filterStudents = (query) => {
   const lowerCaseQuery = query.toLowerCase().trim();
@@ -88,7 +91,7 @@ const filterStudents = (query) => {
     students.value = [...originalStudents.value];
   } else {
     students.value = originalStudents.value.filter((student) =>
-      student.accountNumber.toLowerCase().includes(lowerCaseQuery)
+      student.student.accountNumber.toLowerCase().includes(lowerCaseQuery)
     );
   }
 };
@@ -101,23 +104,38 @@ document.addEventListener("resetFilter", () => {
   students.value = [...originalStudents.value];
 });
 
+// Agregar paginación
+const itemsPerPage = 10;
+const currentPage = ref(1);
+
+const totalPages = computed(() =>
+  Math.ceil(students.value.length / itemsPerPage)
+);
+
+const startIndex = computed(() => (currentPage.value - 1) * itemsPerPage);
+
+const paginatedStudents = computed(() => {
+  return students.value.slice(
+    startIndex.value,
+    startIndex.value + itemsPerPage
+  );
+});
 </script>
 
 <style scoped>
 table {
-    width: 100%;
-    border-collapse: collapse;
+  width: 100%;
+  border-collapse: collapse;
 }
 
 th,
 td {
-    padding: 8px;
-    border-bottom: 2px solid #ddd;
-    text-align: center;
+  padding: 8px;
+  border-bottom: 2px solid #ddd;
+  text-align: center;
 }
 
 th {
-    background-color: #f2f2f2;
+  background-color: #f2f2f2;
 }
-
 </style>
